b5c5ced0f961421c7d9157d83e1c43b9c7aae2d0
DevJag <104650542+DevjagOne@users.noreply.github.com>
2022-05-18 16:14:56 -0300

    hello world algorithm

---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+const x = "Hello World JavaScript";
+let y = "Language JavaScript";
+console.log(x);
+console.log(y);
+y = "Initial project javaScript";
+console.log(y + "\t||\t" + x);
+console.log("\n");
